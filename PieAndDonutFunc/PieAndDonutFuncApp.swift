ea9fd97ebf19761d9f5ade149fcb9859c9eb10e0
Mohit Gupta <gupta.mohit1907@gmail.com>
2023-09-22 07:50:23 +0530

    Initial Commit

---
+++ PieAndDonutFunc/PieAndDonutFuncApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieAndDonutFuncApp.swift
+//  PieAndDonutFunc
+//
+//  Created by Mohit Gupta on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PieAndDonutFuncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
